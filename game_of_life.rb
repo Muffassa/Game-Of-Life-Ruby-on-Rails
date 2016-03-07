44eab51f1a6970fe1a695de0bb73fdcb7f9913ff
Nikita <keddofilatov@gmail.com>
2016-03-07 23:07:06 +0600

    Add world attr row and columns

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,2 +1,5 @@
 class World
+  attr_accessor :rows, :columns
+  def intitialize(rows = 3, columns = 3)
+  end
 end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -7,7 +7,12 @@ describe "Game of life" do
   context "World" do
     subject { World.new }
     it 'should create new world object' do
-      subject.is_a?(World).should be_true
+      subject.is_a?(World).should be_truthy
+    end
+
+    it "should respone the proper methods" do
+      subject.should respond_to(:rows)
+      subject.should respond_to(:columns)
     end
   end
 
