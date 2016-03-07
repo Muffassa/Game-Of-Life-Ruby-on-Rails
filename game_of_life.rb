b0a8419c57d2894573d10e4e927d8df6217dcc24
Nikita <keddofilatov@gmail.com>
2016-03-07 23:30:59 +0600

    Add @cell_grid attr

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,5 +1,12 @@
 class World
-  attr_accessor :rows, :columns
-  def intitialize(rows = 3, columns = 3)
+  attr_accessor :rows, :columns, :cell_grid
+  def initialize(rows = 3, columns = 3)
+    @rows = rows
+    @columns = columns
+
+    @cell_grid = Array.new(3) do |row|
+                Array.new(3) do |columns|
+              end
+            end
   end
 end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -13,6 +13,11 @@ describe "Game of life" do
     it "should respone the proper methods" do
       subject.should respond_to(:rows)
       subject.should respond_to(:columns)
+      subject.should respond_to(:cell_grid)
+    end
+
+    it "should create proper cell grid on initialization" do
+      subject.cell_grid.is_a?(Array).should be_truthy
     end
   end
 
